56d10e8fb234225276135b836a25d1cad4f241c8
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2018-01-03 17:22:55 -0800

    authorization now works on the stuff

--- index.js
+++ index.js
@@ -5,6 +5,4 @@ if (process.env.NODE_ENV !== 'docker') {
 }
 
 require = require('@std/esm')(module); // eslint-disable-line no-global-assign
-
-console.log(process.env.ACCESS_TOKEN);
 require('./server');
--- middlewares/checkAuth.js
+++ middlewares/checkAuth.js
@@ -1,7 +1,39 @@
-import wrap from '../support/wrapAsync';
+const checkAuth = (req, res, next) => {
+    const { ACCESS_TOKEN } = process.env;
 
-const checkAuth = async (req, res, next) => {
-    next();
+    if (req.method === 'POST' && req.body.accessToken) {
+        if (req.body.accessToken.toLowerCase() === ACCESS_TOKEN.toLowerCase()) {
+            next();
+        }
+        else {
+            res.status(403).json({
+                reason: 'Not authorized'
+            });
+        }
+    }
+    else if (req.get('Authorization')) {
+        const authMethod = req.get('Authorization').split(' ')[0];
+        const authToken = req.get('Authorization').split(' ')[1].toLowerCase();
+
+        if (authMethod !== 'Bearer') {
+            res.status(401).json({
+                reason: 'Malformed auth header'
+            });
+        }
+        else if (authToken !== ACCESS_TOKEN.toLowerCase()) {
+            res.status(403).json({
+                reason: 'Not authorized'
+            });
+        }
+        else if (authToken === ACCESS_TOKEN.toLowerCase()) {
+            next();
+        }
+    }
+    else {
+        res.status(401).json({
+            reason: 'No auth token found in request'
+        });
+    }
 };
 
-export default wrap(checkAuth);
+export default checkAuth;
