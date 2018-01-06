d56df564df147ea54075c84a919a95595ed97e19
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2018-01-05 22:48:26 -0800

    wired up the middleware into the server actually

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import morgan from 'morgan';
 import bodyParser from 'body-parser';
 import chalk from 'chalk';
 
-import { checkAuth, getPublicIp } from './middlewares';
+import { checkAuth, getPublicIp, getPoi } from './middlewares';
 
 const port = process.env.PORT || process.argv[2] || 8080;
 const app = express();
@@ -12,6 +12,7 @@ app.use(bodyParser.json());
 app.use(morgan('dev'));
 
 app.get('/net/public-ip', checkAuth, getPublicIp);
+app.get('/net/poi', checkAuth, getPoi);
 
 app.listen(port, () => {
     console.log(`Server now running on port ${chalk.green(port)}`);
