2917834383323bc912818071cd20255131cabdbb
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2018-01-07 08:58:16 -0800

    filled out the get status poi middleware

--- middlewares/getPoiStatus.js
+++ middlewares/getPoiStatus.js
@@ -7,6 +7,16 @@ const pois = require('../poi');
 const getPoiStatus = async (req, res) => {
     const selectedPoi = pois.filter(p => p.id === req.params.id)[0];
     res.send(selectedPoi);
+
+    const responses = selectedPoi.services.map(async (s) => {
+        const r = await request({
+            url: `http://${s.ip}:${s.port}`
+        });
+
+        return { name: s.name, ...r };
+    });
+
+    res.send(responses);
 };
 
 export default wrap(getPoiStatus);
