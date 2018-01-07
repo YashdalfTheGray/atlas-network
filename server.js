2ee3cc943181fd37aefbd6ba08107a50230ef8dc
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2018-01-06 20:15:50 -0800

    added the middleware to the server under /net/poi/status

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import morgan from 'morgan';
 import bodyParser from 'body-parser';
 import chalk from 'chalk';
 
-import { checkAuth, getPublicIp, getPoi } from './middlewares';
+import { checkAuth, getPublicIp, getPoi, getPoiStatus } from './middlewares';
 
 const port = process.env.PORT || process.argv[2] || 8080;
 const app = express();
@@ -13,6 +13,7 @@ app.use(morgan('dev'));
 
 app.get('/net/public-ip', checkAuth, getPublicIp);
 app.get('/net/poi', checkAuth, getPoi);
+app.get('/net/poi/:id', checkAuth, getPoiStatus);
 
 app.listen(port, () => {
     console.log(`Server now running on port ${chalk.green(port)}`);
